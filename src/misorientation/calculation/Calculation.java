0b99efaec6c11ce86741c88dfe5e73ae002fe552
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2011-09-07 12:42:20 -0400

    Bit of code for calculation

--- src/misorientation/calculation/Calculation.java
+++ src/misorientation/calculation/Calculation.java
@@ -1,7 +1,11 @@
 package misorientation.calculation;
 
 import java.io.Writer;
+import java.util.Collections;
 import java.util.List;
+
+import commonenvironment.AlphaNumericComparitor;
+import commonenvironment.IOOperations;
 
 import scitypes.Coord;
 
@@ -9,6 +13,10 @@ public class Calculation {
 
 	public static void calculate(List<String> filenames, Coord<Integer> mapSize, Writer writer)
 	{
+		
+		Collections.sort(filenames, new AlphaNumericComparitor());
+		String commonPrefix = IOOperations.getCommonFileName(filenames);
+		
 		
 	}
 	
